a1f98af5874545f5b48ed32b422280c9f7986390
leandrosouzaa <leandror.dev@gmail.com>
2020-03-26 12:33:22 -0300

    Estilização da Página de Register

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -16,9 +16,9 @@ export default function Register() {
           <h1>Cadastro</h1>
           <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos de sua ONG.</p>
 
-          <Link to="/register" className="back-link">
+          <Link to="/6" className="back-link">
             <FiArrowLeft size={16} color="#E02061" />
-            Não Tenho Cadastro
+            Já tenho cadastro
           </Link>
         </section>
 
