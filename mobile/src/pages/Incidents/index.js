c9e3537d56bfbcd943c7f992fa04b3d48be5c1fb
leandrosouzaa <leandror.dev@gmail.com>
2020-03-26 22:10:11 -0300

    Configuração de Navegação entre telas.

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+export default function Incidents() {
+  return (
+    <View>
+      <Text>Incident</Text>
+    </View>
+  )
+
+}
