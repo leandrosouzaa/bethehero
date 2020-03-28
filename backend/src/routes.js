3ed325ac1d5b6002484710f46d87741d2746fb5d
leandrosouzaa <leandror.dev@gmail.com>
2020-03-28 09:39:41 -0300

    Correção na Validação de Dados (tipo vem antes do required)

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -15,17 +15,28 @@ routes.post('/sessions', SessionController.create);
 
 
 routes.get('/ongs', OngController.index);
+
+
 routes.post('/ongs', celebrate({
-  [Segments.BODY]: Joi.object.keys({
+  [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().required().email(),
     whatsapp: Joi.number().required().min(11),
-    city: Joi.required(),
-    uf: Joi.required().length(2),
+    city: Joi.string().required(),
+    uf: Joi.string().max(2).required()
   })
 }), OngController.store);
 
-routes.post('/incidents', IncidentController.store);
+routes.post('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object().keys({
+    authorization: Joi.string().required()
+  }),
+  [Segments.BODY]: Joi.object().keys({
+    tile: Joi.string().required(),
+    description: Joi.string().required(),
+    value: Joi.number().required(),
+  })
+}), IncidentController.store);
 routes.get('/incidents', celebrate({
   [Segments.QUERY]: Joi.object().keys({
     page: Joi.number()
@@ -33,14 +44,16 @@ routes.get('/incidents', celebrate({
 }), IncidentController.index);
 routes.delete('/incidents/:id', celebrate({
   [Segments.PARAMS]: Joi.object().keys({
-    id: Joi.required().number()
+    id: Joi.number().required(),
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown(),
   })
 }), IncidentController.delete)
-
 routes.get('/profile', celebrate({
-  [Segments.HEADERS]: Joi.object().keys({
-    authorization: Joi.string().required
-  })
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown(),
 }), ProfileController.index);
 
 
